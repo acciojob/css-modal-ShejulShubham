53f8f9cd0148894e50283b5a34df2731cca73a10
ShejulShubham <148786777+ShejulShubham@users.noreply.github.com>
2025-09-20 21:32:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let openModal = document.getElementById("openModal");
+let closeModal = document.getElementById("close-modal");
+let modal = document.getElementById("modal");
+
+function toggleModal(){
+	modal.classList.toggle("show");
+}
+
+modal.onclick = toggleModal;
+
+closeModal.addEventListener("click", toggleModal);
+
+openModal.addEventListener("click", toggleModal);
